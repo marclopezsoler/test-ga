631f022f7ce60071046970fdb49bcd1a5d7500ae
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-13 19:22:05 +0200

    home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import styles from "@/styles/Home.module.scss";
-import { motion } from "framer-motion";
-import { Syne } from "next/font/google";
 import Head from "next/head";
-
-const syne = Syne({ subsets: ["latin"] });
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -18,35 +16,15 @@ export default function Home() {
         <link rel="icon" href="/icon.ico" />
       </Head>
       <motion.div
-        className="container text-center  bg-black"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.35 }}
-      >
-        <main className={`${styles.main} ${syne.className}`}>
-          <section className={styles.home_section_1}>
-            <div className={styles.title_parent}>
-              <h1 className={`${styles.home_title}`}>creative</h1>
-              <div className={styles.title_parent2}>
-                <h1 className={`${styles.home_title}`}>digital </h1>
-                <h1 className={`${styles.home_title}`}>artist</h1>
-              </div>
-            </div>
-            <h2 className={`${styles.home_subtitle}`}>
-              marc lópez is a digital artist and developer from Barcelona, he's
-              keen on creating cool and simple stuff
-            </h2>
-          </section>
-          <section className={styles.home_section_4}>
-            <h2 className={styles.section4_title}>
-              hey! you got to the bottom
-            </h2>
-            <p className={styles.section4_subtitle}>
-              feel free to explore the other sections
-            </p>
-          </section>
-        </main>
+          className="container text-center  bg-black"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.35 }}
+        >
+      <main className={styles.main}>
+        <Link href="/contact">GO TO CONTACT</Link>
+      </main>
       </motion.div>
     </>
   );
