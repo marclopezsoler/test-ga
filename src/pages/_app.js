528bdd93d75cd0d7604e8e82e2457a8e8ac29ac4
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-13 02:43:48 +0200

    ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CYPLVVSN8B);
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B`}
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-CYPLVVSN8B);
-        `}
-        </Script>
-      </Head>
       <body>
         <Main />
         <NextScript />
