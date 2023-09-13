74c1b81c2353a341cb1902c99657df6bd2313d0c
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-13 02:46:38 +0200

    ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-CYPLVVSN8B);
+          gtag('config', 'G-CYPLVVSN8B');
         `}
       </Script>
 
