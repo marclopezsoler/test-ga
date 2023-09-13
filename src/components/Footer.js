73cd797ae6b7a1f702a956edc43c92eee4db8e5c
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-13 18:54:45 +0200

    font titles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import styles from "@/styles/components/Footer.module.scss";
 import Link from "next/link";
+import { Syne } from 'next/font/google'
+ 
+const syne = Syne({ subsets: ['latin'] })
 
 export default function Footer() {
   return (
     <nav className={styles.footer}>
-      <section className={styles.footer_row}>
+      <section className={`${styles.footer_row} ${syne.className}`}>
         <Link href="/" className={styles.footer_link}>Home</Link>
         <Link href="/work" className={styles.footer_link}>Work</Link>
         <Link href="/about" className={styles.footer_link}>About</Link>
@@ -15,7 +18,7 @@ export default function Footer() {
         <a href="https://twitter.com/marclopezsoler" target="_blank" className={styles.footer_link}>Twitter</a>
           <a href="https://files.marclopez.xyz/CV_MarcLopez.pdf" target="_blank" className={styles.footer_link}>CV</a>
       </section>
-      <section className={styles.footer_row}>
+      <section className={`${styles.footer_row} ${syne.className}`}>
         <span className={styles.footer_copy}>© 2023 Portfolio by Marc López</span>
       </section>
     </nav>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@
 import styles from "@/styles/components/Header.module.scss";
 import Link from "next/link";
 import { useState } from "react";
+import { Syne } from 'next/font/google'
+ 
+const syne = Syne({ subsets: ['latin'] })
 
 export default function Header() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -19,7 +22,7 @@ export default function Header() {
     <nav className={`${styles.nav_extended}`}>
       <button className={styles.toggle} onClick={toggleMenu}>{isMenuOpen ? "x" : "m"}</button>
       <div className={` ${isMenuOpen ? styles.show_element : styles.hide_element} ${styles.nav_menu_parent}`}>
-        <div className={styles.nav_menu}>
+        <div className={`${styles.nav_menu} ${syne.className}`}>
           <Link href="/" className={`${styles.menu_item} ${isMenuOpen ? "" : styles.hide1}`} onClick={closeMenu}>home</Link>
           <Link href="/work" className={`${styles.menu_item} ${isMenuOpen ? "" : styles.hide2}`} onClick={closeMenu}>work</Link>
           <Link href="/about" className={`${styles.menu_item} ${isMenuOpen ? "" : styles.hide3}`} onClick={closeMenu}>about</Link>
