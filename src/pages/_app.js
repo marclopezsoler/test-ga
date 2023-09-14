6a80929aebb990e973a3730ac737e4d696efe654
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-14 02:36:08 +0200

    manifest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,28 +3,24 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Popup from "@/components/Popup";
 import "@/styles/globals.scss";
-import Head from "next/head";
 import Script from "next/script";
 import { isMobile } from "react-device-detect";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B`}
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-CYPLVVSN8B');
         `}
-        </Script>
-        <link rel="manifest" href="./manifest.webmanifest" />
-      </Head>
+      </Script>
 
       <Header />
       {isMobile ? <Popup /> : <Cursor />}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,10 @@ import { isMobile } from "react-device-detect";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#020b55" />
+      </Head>
       <body>
         <section className="content">          
           <Main />
