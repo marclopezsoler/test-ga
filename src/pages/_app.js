15b4dd101627d35c0d7e556582a5fcbfb4c1c77d
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-14 02:32:50 +0200

    manifest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,25 +3,28 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Popup from "@/components/Popup";
 import "@/styles/globals.scss";
+import Head from "next/head";
 import Script from "next/script";
 import { isMobile } from "react-device-detect";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B`}
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-CYPLVVSN8B`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-CYPLVVSN8B');
         `}
-      </Script>
-      <link rel="manifest" href="./manifest.webmanifest" />
+        </Script>
+        <link rel="manifest" href="./manifest.webmanifest" />
+      </Head>
 
       <Header />
       {isMobile ? <Popup /> : <Cursor />}
