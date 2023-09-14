230ec3863422208aa91e4404045925b3f4dc105a
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-14 02:54:40 +0200

    manifest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+  // Your other Next.js configurations go here.
+});
