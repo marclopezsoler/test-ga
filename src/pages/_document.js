3b8ab444b05b18a0c647ada78f3b0f7daec8c680
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-14 03:07:14 +0200

    bugs

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,15 @@
-import { Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <link rel="manifest" href="./manifest.json" />
+        <link rel="apple-touch-icon" href="/assets/icons/icon_512.png"></link>
+        <meta name="theme-color" content="#020b55" />
+      </Head>
       <body>
-        <section className="content">
+        <section className="content">          
           <Main />
           <NextScript />
         </section>
