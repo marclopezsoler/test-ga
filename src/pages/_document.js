2d0fdd4136ee05740cc9d7b81610a827fd33ba1c
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-14 02:50:28 +0200

    manifest

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,10 @@
-import Cursor from "@/components/Cursor";
-import Header from "@/components/Header";
 import { Head, Html, Main, NextScript } from "next/document";
-import { isMobile } from "react-device-detect";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="./manifest.json" />
         <link rel="apple-touch-icon" href="/assets/icons/icon_512.png"></link>
         <meta name="theme-color" content="#020b55" />
       </Head>
